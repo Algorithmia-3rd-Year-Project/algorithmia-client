71f94abe0fd1cb4991783a69cec0b5bf934a0e3f
Kavindu <kavindupriyanath@gmail.com>
2023-07-16 23:10:54 +0530

    Basic Navbar implemented

--- frontend/src/views/components/Navbar.js
+++ frontend/src/views/components/Navbar.js
@@ -2,6 +2,11 @@ import { Link } from "react-router-dom";
 import { useLogout } from "../../hooks/useLogout";
 import { useSessionContext } from "../../hooks/useSessionContext";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import icon from "../../images/user.png";
+
 const Navbar = () => {
   const { logout } = useLogout();
   const { user } = useSessionContext();
@@ -12,7 +17,92 @@ const Navbar = () => {
 
   return (
     <header>
-      <div className="container">
+      <div className="content">
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <div className="container">
+            <a href="#" className="navbar-brand">
+              Algorithmia
+            </a>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  News
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Reviews
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Top-Ups
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link"
+                  href="#"
+                  id="profileDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  {!user && <span className="navbar-text">Log In</span>}
+                  {user && <span>{user.email}</span>}
+                  <img
+                    src={icon}
+                    alt=""
+                    className="rounded-circle"
+                    height="25"
+                  />
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="profileDropdown">
+                  {user && (
+                    <>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          My Profile
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Settings
+                        </a>
+                      </li>
+                      <li>
+                        <button onClick={handleClick} className="dropdown-item">
+                          Log out
+                        </button>
+                      </li>
+                    </>
+                  )}
+                  {!user && (
+                    <>
+                      <li>
+                        <Link to="/login" className="dropdown-item">
+                          Login
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/signup" className="dropdown-item">
+                          Sign-Up
+                        </Link>
+                      </li>
+                    </>
+                  )}
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </nav>
         {user && (
           <div>
             <span>{user.email}</span>
@@ -31,7 +121,16 @@ const Navbar = () => {
               <Link to="/signup">
                 <p>Signup</p>
               </Link>
-              <Link to="/">Store</Link>
+
+              <Link to="/">
+                <p>Features</p>
+              </Link>
+              <Link to="/">
+                <p>Reviews</p>
+              </Link>
+              <Link to="/">
+                <p>Top-Ups</p>
+              </Link>
             </div>
           )}
         </div>
