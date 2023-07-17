be78670eed2faef2f532681154ba9803fa3a21c5
Nethmie <nethimanya2000@gmail.com>
2023-07-17 17:05:09 +0530

    changed

---
+++ frontend/src/views/pages/DevlogSingle.js
@@ -0,0 +1,11 @@
+
+const DevlogSingle = () => {
+  return (
+    <div className="home">
+      <h2>Home</h2>
+      <p>Other Pages</p>
+    </div>
+  );
+};
+
+export default DevlogSingle;
--- frontend/src/views/pages/Home.js
+++ frontend/src/views/pages/Home.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import DevlogSingle from "./DevlogSingle";
 
 const Home = () => {
   return (
     <div className="home">
       <h2>Home</h2>
       <p>Other Pages</p>
-      <Link to="/devlogs">See all devlogs</Link>
+      <Link to="/devlogsingle">See one devlog</Link>
     </div>
   );
 };
