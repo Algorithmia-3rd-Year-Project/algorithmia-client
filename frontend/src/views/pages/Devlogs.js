6da55eebb9a32eab02b400a9635e7d38b253b0bb
Nethmie <nethimanya2000@gmail.com>
2023-07-17 17:17:26 +0530

    minor changes

--- frontend/src/views/pages/Devlogs.js
+++ frontend/src/views/pages/Devlogs.js
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 //components
 import DevlogTile from "../components/DevlogTile";
-import icon from "../../images/user.png";
 
 const Devlog = () => {
   const [devlogs, setDevlogs] = useState(null);
@@ -25,13 +22,6 @@ const Devlog = () => {
   return (
     <div className="devlog">
       <h2>Devlogs Page</h2>
-      <div className="devlogs">
-        {devlogs &&
-          devlogs.map((devlog) => (
-            <DevlogTile key={devlog._id} devlog={devlog} />
-          ))}
-      </div>
-
       <div class="container">
         <div class="row">
           <div class="col-lg-6 mb-4">
@@ -46,7 +36,7 @@ const Devlog = () => {
               </ul>
             </div>
           </div>
-          
+
           <div class="col-lg-6 mb-4">
             <div class="dropdown">
               <button class="btn btn-primary btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -61,105 +51,14 @@ const Devlog = () => {
           </div>
         </div>
       </div>
-
-      {/* Should add the below part to DevlogTile component */}
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4 mb-3">
-            <div class="card text-center" style={{ width: 350 }}>
-              <img class="card-img-top" src={icon} alt=".." height="300"/>
-              <div class="card-body">
-                <h5 class="card-title">Devlog Title</h5>
-                <p class="card-text">
-                  Here goes the description of the Devlog
-                </p>
-                <a href="DevlogSingle" class="btn btn-primary btn-sm">View More</a>
-                <div class="card-footer text-muted">35 comments</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6 mb-4">
-            <div class="card text-center" style={{ width: 350 }}>
-              <img class="card-img-top" src={icon} alt=".." height="300"/>
-              <div class="card-body">
-                <h5 class="card-title">Devlog Title</h5>
-                <p class="card-text">
-                  Here goes the description of the Devlog
-                </p>
-                <a href="DevlogSingle" class="btn btn-primary btn-sm">View More</a>
-                <div class="card-footer text-muted">35 comments</div>
-              </div>
-            </div>
-          </div>
+      <div class="row">
+      {devlogs &&
+        devlogs.map((devlog) => (
+          <DevlogTile key={devlog._id} devlog={devlog} />
+        ))}
         </div>
-      </div>
     </div>
   );
 };
 
 export default Devlog;
-
-
-// import { useEffect, useState } from "react";
-
-// //components
-// import DevlogTile from "../components/DevlogTile";
-
-// const Devlog = () => {
-//   const [devlogs, setDevlogs] = useState(null);
-
-//   useEffect(() => {
-//     const fetchDevlogs = async () => {
-//       const response = await fetch("/algorithmia/devlogs");
-//       const json = await response.json();
-
-//       if (response.ok) {
-//         setDevlogs(json);
-//       }
-//     };
-
-//     fetchDevlogs();
-//   }, []);
-
-//   return (
-//     <div className="devlog">
-//       <h2>Devlogs Page</h2>
-//       <div class="container">
-//         <div class="row">
-//           <div class="col-lg-6 mb-4">
-//             <div class="dropdown">
-//               <button class="btn btn-primary btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-//                 Filters
-//               </button>
-//               <ul class="dropdown-menu">
-//                 <li><a class="dropdown-item" href="#">Filter 1</a></li>
-//                 <li><a class="dropdown-item" href="#">Filter 2</a></li>
-//                 <li><a class="dropdown-item" href="#">Filter3</a></li>
-//               </ul>
-//             </div>
-//           </div>
-
-//           <div class="col-lg-6 mb-4">
-//             <div class="dropdown">
-//               <button class="btn btn-primary btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-//                 Sort
-//               </button>
-//               <ul class="dropdown-menu">
-//                 <li><a class="dropdown-item" href="#">A-Z Order</a></li>
-//                 <li><a class="dropdown-item" href="#">By Date</a></li>
-//                 <li><a class="dropdown-item" href="#">By Title</a></li>
-//               </ul>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-
-//       {devlogs &&
-//         devlogs.map((devlog) => (
-//           <DevlogTile key={devlog._id} devlog={devlog} />
-//         ))}
-//     </div>
-//   );
-// };
-
-// export default Devlog;
--- frontend/src/views/pages/Home.js
+++ frontend/src/views/pages/Home.js
@@ -8,134 +8,134 @@ const Home = () => {
       <p>Other Pages</p>
       <Link to="/devlogs">See all devlogs</Link>
 
-  <div class="container-fluid my-5">
-    <div class="row p-5 pb-0 align-items-center rounded-3 border text-center mx-5">
       <div class="container-fluid my-5">
-        <div class="row p-5 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border text-center mx-5 bg-success">
-          <div class=" p-5 p-lg-5 pt-lg-3">
-            <h1 class="display-4 fw-bold lh-1 text-body-emphasis">Algorithmia</h1>
-            <p class="lead mx-5">
-              Embark on an exciting educational adventure with DSA Quest, the ultimate game that turns
-              learning Data Structures and Algorithms (DSA) into an immersive and enjoyable experience!
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="container my-5">
-      <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-center">About</h1>
-        <div class="row p-5 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border text-center mx-5">
-          <div class=" p-5 p-lg-5 pt-lg-3">
-            <p class="lead mx-5">
-              Embark on an exciting educational adventure with DSA Quest, the ultimate 
-              game that turns learning Data Structures and Algorithms (DSA) into an immersive 
-              and enjoyable experience!. Dive into a captivating virtual world where you'll 
-              unravel the secrets of DSA through thrilling challenges, mind-bending puzzles, and strategic gameplay.
-            </p>
-            <div class="d-grid gap-2 d-md-flex justify-content-center mb-4 mb-lg-3">
-              <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Download</button>
-              <button type="button" class="btn btn-outline-secondary btn-lg px-4">Download</button>
+        <div class="row p-5 pb-0 align-items-center rounded-3 border text-center mx-5">
+          <div class="container-fluid my-5">
+            <div class="row p-5 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border text-center mx-5 bg-success">
+              <div class=" p-5 p-lg-5 pt-lg-3">
+                <h1 class="display-4 fw-bold lh-1 text-body-emphasis">Algorithmia</h1>
+                <p class="lead mx-5">
+                  Embark on an exciting educational adventure with DSA Quest, the ultimate game that turns
+                  learning Data Structures and Algorithms (DSA) into an immersive and enjoyable experience!
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <div class="container my-5">
-      <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-center">Patch Highlights</h1>
-        <div class="row p-5 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border mx-5">
-          <div class="col-lg-7 p-5 p-lg-5 pt-lg-3">
-            <p class="lead mx-5">
-              Added a new interactive visualization mode for sorting algorithms, allowing players to 
-              visually understand the step-by-step process of popular sorting techniques like bubble sort, 
-              merge sort, and quicksort.
-            </p>
-          </div>
-          <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden mx-5">
-              <img class="images/patch1.jpg" src="bootstrap-docs.png" alt="" width="720"/>
-          </div>
-        </div>
-      </div>
-
-      <div class="container my-5">
-      <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-center">Game Features</h1>
-        <div class="row p-5 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border mx-5">
-        <div class="container px-5 text-center">
-          <img src="bootstrap-docs.png" class="img-fluid border rounded-3 mb-4" alt="Example image" width="700" height="500" loading="lazy"/>
-        </div>
-            <div class="d-grid gap-2 d-md-flex justify-content-center mb-4 mb-lg-3">
-              <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">News</button>
-              <button type="button" class="btn btn-outline-secondary btn-lg px-4">Features</button>
-            </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  {/* footer */}
-  <div class="container-fluid my-5">
-    <div class="row p-4 pb-0 align-items-center rounded-3 border text-center mx-5">
-      <div class="container">
-        <footer class="py-5">
-          <div class="row">
-            <div class="col-6 col-md-2 mb-3">
-              <h5>Section</h5>
-              <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-              </ul>
-            </div>
-
-            <div class="col-6 col-md-2 mb-3">
-              <h5>Section</h5>
-              <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-              </ul>
-            </div>
-
-            <div class="col-6 col-md-2 mb-3">
-              <h5>Section</h5>
-              <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-              </ul>
-            </div>
-
-            <div class="col-md-5 offset-md-1 mb-3">
-              <form>
-                <h5>Subscribe to our newsletter</h5>
-                <p>Monthly digest of what's new and exciting from us.</p>
-                <div class="d-flex flex-column flex-sm-row w-100 gap-2">
-                  <label for="newsletter1" class="visually-hidden">Email address</label>
-                  <input id="newsletter1" type="text" class="form-control" placeholder="Email address"/>
-                  <button class="btn btn-primary" type="button">Subscribe</button>
+          <div class="container my-5">
+            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-center">About</h1>
+            <div class="row p-5 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border text-center mx-5">
+              <div class=" p-5 p-lg-5 pt-lg-3">
+                <p class="lead mx-5">
+                  Embark on an exciting educational adventure with DSA Quest, the ultimate
+                  game that turns learning Data Structures and Algorithms (DSA) into an immersive
+                  and enjoyable experience!. Dive into a captivating virtual world where you'll
+                  unravel the secrets of DSA through thrilling challenges, mind-bending puzzles, and strategic gameplay.
+                </p>
+                <div class="d-grid gap-2 d-md-flex justify-content-center mb-4 mb-lg-3">
+                  <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Download</button>
+                  <button type="button" class="btn btn-outline-secondary btn-lg px-4">Download</button>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
 
-          <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-            <p>© 2023 Company, Inc. All rights reserved.</p>
-            <ul class="list-unstyled d-flex">
-              <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use href="#twitter"></use></svg></a></li>
-              <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use href="#instagram"></use></svg></a></li>
-              <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use href="#facebook"></use></svg></a></li>
-            </ul>
+          <div class="container my-5">
+            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-center">Patch Highlights</h1>
+            <div class="row p-5 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border mx-5">
+              <div class="col-lg-7 p-5 p-lg-5 pt-lg-3">
+                <p class="lead mx-5">
+                  Added a new interactive visualization mode for sorting algorithms, allowing players to
+                  visually understand the step-by-step process of popular sorting techniques like bubble sort,
+                  merge sort, and quicksort.
+                </p>
+              </div>
+              <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden mx-5">
+                <img class="images/patch1.jpg" src="bootstrap-docs.png" alt="" width="720" />
+              </div>
+            </div>
           </div>
-        </footer>
+
+          <div class="container my-5">
+            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3 text-center">Game Features</h1>
+            <div class="row p-5 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border mx-5">
+              <div class="container px-5 text-center">
+                <img src="bootstrap-docs.png" class="img-fluid border rounded-3 mb-4" alt="Example image" width="700" height="500" loading="lazy" />
+              </div>
+              <div class="d-grid gap-2 d-md-flex justify-content-center mb-4 mb-lg-3">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">News</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Features</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-      
+
+      {/* footer */}
+      <div class="container-fluid my-5">
+        <div class="row p-4 pb-0 align-items-center rounded-3 border text-center mx-5">
+          <div class="container">
+            <footer class="py-5">
+              <div class="row">
+                <div class="col-6 col-md-2 mb-3">
+                  <h5>Section</h5>
+                  <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+                  </ul>
+                </div>
+
+                <div class="col-6 col-md-2 mb-3">
+                  <h5>Section</h5>
+                  <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+                  </ul>
+                </div>
+
+                <div class="col-6 col-md-2 mb-3">
+                  <h5>Section</h5>
+                  <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+                  </ul>
+                </div>
+
+                <div class="col-md-5 offset-md-1 mb-3">
+                  <form>
+                    <h5>Subscribe to our newsletter</h5>
+                    <p>Monthly digest of what's new and exciting from us.</p>
+                    <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                      <label for="newsletter1" class="visually-hidden">Email address</label>
+                      <input id="newsletter1" type="text" class="form-control" placeholder="Email address" />
+                      <button class="btn btn-primary" type="button">Subscribe</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+
+              <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                <p>© 2023 Company, Inc. All rights reserved.</p>
+                <ul class="list-unstyled d-flex">
+                  <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use href="#twitter"></use></svg></a></li>
+                  <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use href="#instagram"></use></svg></a></li>
+                  <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use href="#facebook"></use></svg></a></li>
+                </ul>
+              </div>
+            </footer>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
