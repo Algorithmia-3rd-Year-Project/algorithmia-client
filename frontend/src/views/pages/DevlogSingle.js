c32b96970a4cbf05c5f6cdfb5fc9f01258e226ba
Nethmie <nethimanya2000@gmail.com>
2023-07-19 23:55:19 +0530

    backend

--- frontend/src/views/pages/DevlogSingle.js
+++ frontend/src/views/pages/DevlogSingle.js
@@ -3,11 +3,29 @@ import "bootstrap/dist/css/bootstrap.css";
 import icon from "../../images/user.png";
 import image from "../../images/beach.webp";
 
+import { useEffect, useState } from "react";
+
 const DevlogSingle = () => {
+  const [devlog, setDevlog] = useState(null);
+
+  useEffect(() => {
+    const fetchDevlog = async () => {
+      const response = await fetch("/algorithmia/devlog/649925302740d709ec4399fb");
+      const json = await response.json();
+
+      if (response.ok) {
+        setDevlog(json);
+      }
+    };
+
+    fetchDevlog();
+  }, []);
+
   return (
-    <div className="container mt-3 pt-3 border rounded-4" style={{backgroundColor: "#ACDBDF"}}>
-      <h2 className="fw-bold" style={{color: "#002B5B"}}>Devlog Title</h2>
-      <h5 className="text-muted">Published 23/09/2022</h5><hr/>
+    <div className="container mt-3 pt-3 border rounded-4" style={{ backgroundColor: "#ACDBDF" }}>
+      <h2 className="fw-bold" style={{ color: "#002B5B" }}>{devlog.title}</h2>
+      <h5 className="text-muted">Published 23/09/2022</h5>
+      <hr />
       <div className="row pt-3">
         <div className="col-12 col-md-6 col-lg-4">
           <img
@@ -36,6 +54,7 @@ const DevlogSingle = () => {
 
       <section>
         <p>
+          {devlog.content}
           Hey there, fellow adventurers!
 
           Welcome to the first edition of our game development devlog, where we'll be sharing behind-the-scenes details, progress updates, and exciting insights into the making of our upcoming game. Join us on this thrilling journey as we craft an unforgettable gaming experience.
@@ -52,12 +71,12 @@ const DevlogSingle = () => {
           One of the key pillars of our game's design is procedural generation, allowing for an endlessly diverse and ever-changing world. We've been hard at work developing a robust procedural generation system that creates unique landscapes, dungeons, and encounters for players to explore.
           Our algorithmic wizardry generates stunning vistas, treacherous mountain ranges, and sprawling forests, each teeming with secrets and challenges. We want players to feel a sense of awe and discovery every time they venture into uncharted territory.
         </p>
-      </section><hr/>
+      </section><hr />
 
       <section>
         <div className="container my-5">
           <div className="row">
-          <h5>Comments</h5>
+            <h5>Comments</h5>
             <div className="d-flex flex-start w-75">
               <img className="rounded-circle shadow-1-strong me-3"
                 src={icon} alt="avatar" width="65"
@@ -76,7 +95,7 @@ const DevlogSingle = () => {
                 </div>
               </div>
             </div>
-          </div><br/><br/><br/>
+          </div><br /><br /><br />
 
           <div className="row">
             <div className="d-flex flex-start w-75">
@@ -97,7 +116,7 @@ const DevlogSingle = () => {
                 </div>
               </div>
             </div>
-          </div><br/><br/><br/>
+          </div><br /><br /><br />
 
           <div className="row">
             <div className="d-flex flex-start w-75">
