18db15ed0c80e07901f28d4adada0f34b8d47755
Himash <klhimash@gmail.com>
2023-07-19 14:17:12 +0530

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/assets/css/gameDashboard.css 	modified:   frontend/src/views/pages/Admin/gameDashboard.js

--- frontend/src/views/pages/Admin/gameDashboard.js
+++ frontend/src/views/pages/Admin/gameDashboard.js
@@ -9,7 +9,7 @@ const GameDashboard = () => {
             <div className="container">
                 <div className="row" style={{marginTop:'60px'}}>
                     <div class="col-xl-4 col-lg-6">
-                        <div class="card l-bg-cherry">
+                        <div class="card l-bg-green">
                             <div class="card-statistic-3 p-4">
                                 <div class="card-icon card-icon-large">
                                     <i class="fas fa-download"></i>
@@ -35,7 +35,7 @@ const GameDashboard = () => {
                     </div>
 
                     <div class="col-xl-4 col-lg-6">
-                        <div class="card l-bg-blue-dark">
+                        <div class="card l-bg-green">
                             <div class="card-statistic-3 p-4">
                                 <div class="card-icon card-icon-large"><i class="fas fa-eye"></i></div>
                                 <div class="mb-4">
@@ -52,14 +52,14 @@ const GameDashboard = () => {
                                     </div>
                                 </div>
                                 <div class="progress mt-1 " data-height="8" style={{ height: '8px' }}>
-                                    <div class="progress-bar l-bg-green" role="progressbar" data-width="25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{ width: '30%' }}></div>
+                                    <div class="progress-bar l-bg-cyan" role="progressbar" data-width="25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{ width: '30%' }}></div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     
                     <div class="col-xl-4 col-lg-6">
-                        <div class="card l-bg-green-dark">
+                        <div class="card l-bg-green">
                             <div class="card-statistic-3 p-4">
                                 <div class="card-icon card-icon-large"><i class="fas fa-dollar-sign"></i></div>
                                 <div class="mb-4">
@@ -84,20 +84,60 @@ const GameDashboard = () => {
                 </div>
                 
                 <div className="row">
-                    <div class="card text-white bg-secondary mb-2" style={{ maxWidth: '25rem', marginLeft: '175px' }}>
+
+                    <div class="col-xl-4 col-lg-6" style={{ maxWidth: '40rem', marginLeft: '100px' }}>
+                        <div class="card l-bg-blue-dark">
+                            <div class="card-statistic-3 p-4">
+                                <div class="card-icon card-icon-large"><i class="fas fa-tape"></i></div>
+                                <div class="mb-4">
+                                    <h5 class="card-title mb-0">Stickiness Rate</h5>
+                                </div>
+                                <div class="row align-items-center mb-2 d-flex">
+                                    <div class="col-8">
+                                        <h2 class="d-flex align-items-center mb-0">
+                                            34%
+                                        </h2>
+                                    </div>
+                                    
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                
+                    {/* <div class="card text-white bg-secondary mb-2" style={{ maxWidth: '25rem', marginLeft: '175px' }}>
                         <div className="col">
                             <h3 class="card-header">Stickiness Rate</h3>
                             <div class="card-body">
                                 <h4 class="card-title" style={{ textAlign: 'center' }}>24%</h4>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
-                    <div class="card text-white bg-secondary mb-2" style={{ maxWidth: '25rem', marginLeft: '100px' }}>
+                    {/* <div class="card text-white bg-secondary mb-2" style={{ maxWidth: '25rem', marginLeft: '100px' }}>
                         <div className="col">
                             <h3 class="card-header">Retention Rate</h3>
                             <div class="card-body">
                                 <h4 class="card-title" style={{ textAlign: 'center' }}>35%</h4>
+                            </div>
+                        </div>
+                    </div> */}
+
+                    <div class="col-xl-4 col-lg-6" style={{ maxWidth: '40rem', marginLeft: '100px' }}>
+                        <div class="card l-bg-blue-dark">
+                            <div class="card-statistic-3 p-4">
+                                <div class="card-icon card-icon-large"><i class="fas fa-chart-line"></i></div>
+                                <div class="mb-4">
+                                    <h5 class="card-title mb-0">Retention Rate</h5>
+                                </div>
+                                <div class="row align-items-center mb-2 d-flex">
+                                    <div class="col-8">
+                                        <h2 class="d-flex align-items-center mb-0">
+                                            26%
+                                        </h2>
+                                    </div>
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
