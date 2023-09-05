6fff9f62014b1fa402c0e430f0141f56490d0c72
shashdm <shashinivigoo@gmail.com>
2023-09-05 19:10:19 +0530

    edit and delete buttons added

--- frontend/src/views/pages/Reviews.js
+++ frontend/src/views/pages/Reviews.js
@@ -43,118 +43,151 @@ const Review = () => {
   //backend
   const [reviews, setReviews] = useState(null);
 
-  useEffect(() => {
-    const fetchReviews = async () => {
-      try {
-        const response = await fetch("/algorithmia/reviews");
-        const json = await response.json();
+  const fetchReviews = async () => {
+    try {
+      const response = await fetch("/algorithmia/reviews");
+      const json = await response.json();
 
-        if (response.ok) {
-          setReviews(json);
-          checkUserReviews(json);
-        }
-      } catch (error) {
-      } finally {
-        setLoading(false);
+      if (response.ok) {
+        setReviews(json);
+        //checkUserReviews(json);
       }
-    };
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     fetchReviews();
-  }, []);
 
-  const checkUserReviews = (reviewList) => {
-    reviewList.forEach((obj) => {
-      Object.entries(obj).forEach(([key, value]) => {
-        if (key === "name" && value === user.email) {
+    if (reviews !== null) {
+      reviews.forEach((element) => {
+        if (element.name === user.email) {
           setUserHasNotReviews(false);
         }
       });
-    });
-  };
+    }
+  }, []);
+
+  // const checkUserReviews = (reviewList) => {
+  //   reviewList.forEach((obj) => {
+  //     Object.entries(obj).forEach(([key, value]) => {
+  //       if (key === "name" && value === user.email) {
+  //         setUserHasNotReviews(false);
+  //       }
+  //     });
+  //   });
+  // };
 
   if (loading) {
     return <p>loading...</p>;
   }
 
   return (
-    <section
-      class="p-4 p-md-5 text-center text-lg-start shadow-1-strong"
-      style={{ backgroundColor: "#002b5b" }}
-    >
-      <div className="text-center" style={{ color: "white" }}>
-        <h1>Reviews</h1>
-      </div>
-      <div class="row d-flex justify-content-center">
-        <div class="col-md-10">
-          <div className="d-flex justify-content-end">
-            {userHasReviews && (
+    <>
+      <section
+        class="p-4 p-md-5 text-center text-lg-start shadow-1-strong"
+        style={{ backgroundColor: "#002b5b" }}
+      >
+        <div className="text-center" style={{ color: "white" }}>
+          <h1>Reviews</h1>
+        </div>
+        <div class="row d-flex justify-content-center">
+          <div class="col-md-10">
+            <div className="d-flex justify-content-end">
+            <li class="list-inline-item">
               <button
+                class="btn btn-success btn-sm rounded-0"
                 type="button"
-                className="btn btn-primary btn-lg px-4 me-md-2 fw-bold"
-                style={{
-                  backgroundColor: "#1a5f7a",
-                  color: "white",
-                  borderColor: "#1a5f7a",
-                }}
-                onClick={handleShowModal}
+                data-toggle="tooltip"
+                data-placement="top"
+                title="Edit"
               >
-                Add Review
+                <i class="fa fa-edit"></i>
               </button>
-            )}
-          </div>
+              </li>
+              <button
+                class="btn btn-danger btn-sm rounded-0"
+                type="button"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="Delete"
+              >
+                <i class="fa fa-trash"></i>
+              </button>
+              {userHasNotReviews && (
+                <button
+                  type="button"
+                  className="btn btn-primary btn-lg px-4 me-md-2 fw-bold"
+                  style={{
+                    backgroundColor: "#1a5f7a",
+                    color: "white",
+                    borderColor: "#1a5f7a",
+                  }}
+                  onClick={handleShowModal}
+                >
+                  Add Review
+                </button>
+              )}
+            </div>
 
-          <br></br>
-          <br></br>
+            <br></br>
+            <br></br>
 
-          {reviews &&
-            reviews.map((review, index) => (
-              <React.Fragment>
-                <div className="card" style={{ backgroundColor: "#acdbdf" }}>
-                  <div className="card-body m-3">
-                    <div className="row">
-                      <div className="col-lg-4 d-flex justify-content-center align-items-center mb-4 mb-lg-0">
-                        <img
-                          src={profile}
-                          className="rounded-circle img-fluid shadow-1"
-                          alt="woman avatar"
-                          width="150"
-                          height="150"
-                        />
-                      </div>
-                      <div className="col-lg-8">
-                        <p className="fw-bold lead mb-2">
-                          <strong>{review.name}</strong>
+            {reviews &&
+              reviews.map((review, index) => (
+                <React.Fragment>
+                  <div className="card" style={{ backgroundColor: "#acdbdf" }}>
+                    <div className="card-body m-3">
+                      <div className="row">
+                        <div className="col-lg-4 d-flex justify-content-center align-items-center mb-4 mb-lg-0">
+                          <img
+                            src={profile}
+                            className="rounded-circle img-fluid shadow-1"
+                            alt="woman avatar"
+                            width="150"
+                            height="150"
+                          />
+                        </div>
+                        <div className="col-lg-8">
+                          <p className="fw-bold lead mb-2">
+                            <strong>{review.name}</strong>
 
-                          <br></br>
-                          {[...Array(review.rate)].map((_, index) => (
-                            <FontAwesomeIcon
-                              icon={solidStar}
-                              style={{ color: "#b78700" }}
-                            />
-                          ))}
-                          {[...Array(5 - review.rate)].map((_, index) => (
-                            <FontAwesomeIcon
-                              icon={regularStar}
-                              style={{ color: "#b78700" }}
-                            />
-                          ))}
-                        </p>
-                        <br />
-                        <p className="mb-4">{review.content}</p>
+                            <br></br>
+                            {[...Array(review.rate)].map((_, index) => (
+                              <FontAwesomeIcon
+                                icon={solidStar}
+                                style={{ color: "#b78700" }}
+                              />
+                            ))}
+                            {[...Array(5 - review.rate)].map((_, index) => (
+                              <FontAwesomeIcon
+                                icon={regularStar}
+                                style={{ color: "#b78700" }}
+                              />
+                            ))}
+                          </p>
+                          <br />
+                          <p className="mb-4">{review.content}</p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <br></br>
-              </React.Fragment>
-            ))}
+                  <br></br>
+                </React.Fragment>
+              ))}
 
-          <br></br>
-          <br></br>
+            <br></br>
+            <br></br>
+          </div>
         </div>
-      </div>
-      <ReviewModal showModal={showModal} handleCloseModal={handleCloseModal} />
-    </section>
+        <ReviewModal
+          showModal={showModal}
+          handleCloseModal={handleCloseModal}
+        />
+      </section>
+    </>
   );
 };
 
